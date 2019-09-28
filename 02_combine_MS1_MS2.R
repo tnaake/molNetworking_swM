026b54235ddd769d1d078d1f9c37d50ef3c27380
tnaake <thomasnaake@gmx.de>
2019-09-28 21:45:06 +0200

    update 02_combine_MS1_MS2.R

--- 02_combine_MS1_MS2.R
+++ 02_combine_MS1_MS2.R
@@ -6,12 +6,9 @@
 ## read MS1 peaklist 
 setwd("/home/thomas/Projects/molNetworking_swM/results_MS1/")
 ## neg
-pl_neg_swM <- read.table("", sep = "\t")
+pl_neg_swM <- read.table("peaklist_neg_log_tic_batch_final_swM.txt", sep = "\t")
 ## pos
-pl_pos_swM <- read.table("", sep = "\t")
-
-peaklist <- read.table("peaklist_log_batch_tic_final.txt", sep = "\t")
-
+pl_pos_swM <- read.table("peaklist_pos_log_tic_batch_final_swM.txt", sep = "\t")
 
 ## load Spectrum2 and Spectra objects
 setwd("/home/thomas/Projects/molNetworking_swM/results_MS2/")
@@ -20,8 +17,6 @@ load("ms2_spectra_swM_hcd_neg.RData")
 load("ms2_spectra_swM_cid_pos.RData")
 load("ms2_spectra_swM_hcd_pos.RData")
 
-## iterate through each Spectrum2 
-spectra <- spectra_swM_cid_neg
 #' @name map_precursor
 #' @title Map precursors of MS2 to MS1
 #' @description The function `map_precursor` takes a `Spectra` objects
@@ -80,13 +75,26 @@ map_precursor <- function(spectra, peaklist, ppm = 20, rt_tol = 0.15, rt_ms1 = c
 
 ## sweet maize CID, negative
 pl_cid_neg_map <- map_precursor(spectra_swM_cid_neg, pl_neg_swM, ppm = 20, 
-    rt_tol = 0.15, rt_ms1 = "seconds")
+    rt_tol = 0.20, rt_ms1 = "seconds")
 ## sweet maize HCD, negative
 pl_hcd_neg_map <- map_precursor(spectra_swM_hcd_neg, pl_neg_swM, ppm = 20, 
-    rt_tol = 0.15, rt_ms1 = "seconds")
+    rt_tol = 0.20, rt_ms1 = "seconds")
+## take for positive mode a higher rt_tol since retention time deviation
+## occured for one sample
 ## sweet maize CID, positive
 pl_cid_pos_map <- map_precursor(spectra_swM_cid_pos, pl_pos_swM, ppm = 20, 
-    rt_tol = 0.15, rt_ms1 = "seconds")
+    rt_tol = 0.35, rt_ms1 = "seconds")
 ## sweet maize HCD, positive
 pl_hcd_pos_map <- map_precursor(spectra_swM_hcd_pos, pl_pos_swM, ppm = 20, 
-    rt_tol = 0.15, rt_ms1 = "seconds")
+    rt_tol = 0.35, rt_ms1 = "seconds")
+
+## write the peaklist table to a file
+setwd("/home/thomas/Projects/molNetworking_swM/results_MS1/")
+write.table(pl_cid_neg_map, file = "peaklist_neg_swM_cid_map.txt", sep = "\t",
+    quote = FALSE, dec = ".")
+write.table(pl_hcd_neg_map, file = "peaklist_neg_swM_hcd_map.txt", sep = "\t",
+    quote = FALSE, dec = ".")
+write.table(pl_cid_pos_map, file = "peaklist_pos_swM_cid_map.txt", sep = "\t",
+    quote = FALSE, dec = ".")
+write.table(pl_hcd_pos_map, file = "peaklist_pos_swM_hcd_map.txt", sep = "\t",
+        quote = FALSE, dec = ".")
